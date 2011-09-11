f74dab498b919e7d3fcc01f800be7afc5497c464
Seth N. Hetu <seth.hetu@gmail.com>
2011-09-12 00:07:48 +0700

    Example 4

--- MapView/src/VariousExamples.hpp
+++ MapView/src/VariousExamples.hpp
@@ -17,19 +17,25 @@ struct Application : Window {
   Button btn1;
   Button btn2;
   Button btn3;
+  Button btn4;
+  Button btn5;
 
   void create() {
     setTitle("Test Application");
-    setGeometry({ 128, 128, 500, 150 });
+    setGeometry({ 128, 128, 300, 300 });
 
-    btn1.setText("Open");
-    btn2.setText("Save");
-    btn3.setText("Quit");
+    btn1.setText("Big!");
+    btn2.setText("North");
+    btn3.setText("South");
+    btn4.setText("West");
+    btn5.setText("East");
 
     layout.setMargin(5);
-    layout.append(btn1, {{0.0}}, {{0.0}});
-    layout.append(btn2, {{btn1, 0, Anchor::Center}}, {{btn1, 5}});
-    layout.append(btn3, {{btn2, 0, Anchor::Center}}, {{btn2, 5}});
+    layout.append(btn2, {Centered, {btn1}}, {{}, {btn1, -5}});
+    layout.append(btn3, {Centered, {btn1}}, {{btn1, 5}});
+    layout.append(btn4, {{}, {btn1, -5}}, {Centered, {btn1}});
+    layout.append(btn5, {{btn1, 5}}, {Centered, {btn1}});
+    layout.append(btn1, {Centered, {0.5}, 100}, {Centered, {0.5}, 100});
     append(layout);
 
     onClose = [&layout] {
