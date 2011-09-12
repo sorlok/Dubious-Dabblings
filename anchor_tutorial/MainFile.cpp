ba81f4ac0777ca57686fec947a7a0286d407bd12
Seth N. Hetu <seth.hetu@gmail.com>
2011-09-12 15:37:18 +0700

    Example 5

--- anchor_tutorial/MainFile.cpp
+++ anchor_tutorial/MainFile.cpp
@@ -12,6 +12,7 @@ AnchorPoint Centered = Axis::Centered();
 struct Application : Window {
   AnchorLayout layout;
   Button okButton;
+  Button fancyButton;
   Button quitButton;
 
   void create() {
@@ -19,12 +20,13 @@ struct Application : Window {
     setGeometry({ 128, 128, 640, 480 });
 
     okButton.setText("Ok"); 
+    fancyButton.setText("Fancy");
     quitButton.setText("Quit");
 
-  layout.setMargin(5);
-  Axis vert = {{0.0},{0.0,75}};
-  layout.append(okButton, {{0.0},{0.0,150}}, vert);
-  layout.append(quitButton, {{1.0, -150},{1.0}}, vert);
+    layout.setMargin(5);
+    layout.append(okButton, {{0.0}}, {{0.0}});
+    layout.append(quitButton, {{}, {1.0}}, {{0.0}});
+    layout.append(fancyButton, {{okButton, 5},{quitButton, -5}}, {{0.0}});
 
     append(layout);
 
