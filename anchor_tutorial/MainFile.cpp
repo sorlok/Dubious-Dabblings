24c208219491a7433ddf8c517b3374aaa0f1ee29
Seth N. Hetu <seth.hetu@gmail.com>
2011-09-12 11:54:32 +0700

    First tutorial.

--- anchor_tutorial/MainFile.cpp
+++ anchor_tutorial/MainFile.cpp
@@ -11,13 +11,31 @@ AnchorPoint Centered = Axis::Centered();
 
 struct Application : Window {
   AnchorLayout layout;
+  Button okButton;
   Button quitButton;
 
   void create() {
     setTitle("Anchor Layout Tutorial");
     setGeometry({ 128, 128, 640, 480 });
+
+    okButton.setText("Ok"); 
     quitButton.setText("Quit");
+
+    AnchorPoint okLeft(0.0);
+    AnchorPoint okRight = {};
+    AnchorPoint quitLeft = {};
+    AnchorPoint quitRight(1.0);
+    AnchorPoint bothTop(0.0);
+    AnchorPoint bothBottom = {};
     layout.append(quitButton, {Centered, {0.5}}, {Centered, {0.5}});
+
+    Axis okHorizontal(okLeft, okRight);
+    Axis quitHorizontal(quitLeft, quitRight);
+    Axis bothVertical(bothTop, bothBottom);
+
+    layout.append(okButton, okHorizontal, bothVertical);
+    layout.append(quitButton, quitHorizontal, bothVertical);
+
     append(layout);
 
     onClose = quitButton.onTick = [] {
