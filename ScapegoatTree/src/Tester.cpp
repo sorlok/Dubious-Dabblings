8dc04217c674b38e53cbd8666b50fe0fef46aba4
Seth N. Hetu <seth.hetu@gmail.com>
2011-09-20 15:19:36 +0800

    Fast log class. Performs even faster if the base is an integer.

--- ScapegoatTree/src/Tester.cpp
+++ ScapegoatTree/src/Tester.cpp
@@ -1,12 +1,14 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 
 #include <iostream>
 #include <fstream>
 #include <list>
 
 #include "scapegoat.hpp"
+#include "fastlog.hpp"
 
 using namespace std;
 
@@ -57,14 +59,14 @@ void deleteRandom(lightweight_map<int, int>& mymap, double amt)
 int main()
 {
 	//Load random.
-	lightweight_map<int, int> mymap;
+/*	lightweight_map<int, int> mymap;
 	loadRandom(mymap, 100);
 
 	//Now randomly delete a certain percentage of that tree.
 	deleteRandom(mymap, 0.2);
 
 	//Add a few more
-	loadRandom(mymap, 10);
+	loadRandom(mymap, 10);*/
 
 	//Specific tests
 	/*list<int> add_order = {5, 37, 13, 58, 89, 34, 90, 96, 29, 84, 92, 40, 42, 22, 51, 3, 15, 2, 31, 47, 59, 20, 66, 14, 72, 68, 9, 88, 12, 86, 62, 75, 82, 61, 91, 81, 67, 71, 26, 35, 23, 17, 95, 16, 30, 6, 50, 21, 46, 24, 78, 93, 79, 19, 18, 11, 25, 76, 77, 28, 4, 48, 44, 56, 69, 45, 49, 63, 94, 1, 55, 10, 43, 32, 33, 36, 85, 98, 70, 74, 65, 73, 60, 64, 39, 38, 83, 54, 52, 100, 41, 8, 97, 99, 57, 27, 80, 7, 87, 53};
@@ -78,8 +80,38 @@ int main()
 	}*/
 
 	//Print a GraphVis input file for this tree.
-	if (!mymap.printDot("tree.dot")) {
+	/*if (!mymap.printDot("tree.dot")) {
 		std::cout <<"Error: Couldn't print file.\n";
+	}*/
+
+
+	//Fast log test
+	float alpha = 1.0F / 0.55F;
+	fast_log<int> logB2(2);
+	fast_log<float> logAlpha(alpha);
+	for (size_t i=0; i<1000; i++) {
+		int toCheckI = rand() % 1000+1;
+		float toCheckF = toCheckI / 0.33F;
+
+		//Get actual logs
+		float actLog2 = log2(toCheckI);
+		float actLogAlpha = log(toCheckF)/log(alpha);
+
+		//Compare floor/ceiling: log2
+		if (logB2.floor(toCheckI) != ((int)floor(actLog2))) {
+			std::cout <<"Floor<int> mismtach.\n";
+		}
+		if (logB2.ceil(toCheckI) != ((int)ceil(actLog2))) {
+			std::cout <<"Ceiling<int> mismtach.\n";
+		}
+
+		//Compare floor/ceiling: logAlpha
+		if (logAlpha.floor(toCheckF) != ((int)floor(actLogAlpha))) {
+			std::cout <<"Floor<float> mismtach.\n";
+		}
+		if (logAlpha.ceil(toCheckF) != ((int)ceil(actLogAlpha))) {
+			std::cout <<"Ceiling<float> mismtach.\n";
+		}
 	}
 
 
