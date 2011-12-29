6e4b2128afa62bfcfae31a51dbc0e93448bdfdb0
Seth N. Hetu <seth.hetu@gmail.com>
2011-12-29 08:05:58 +0800

    Cmake builds; regular make fails. Added .gitignore nonsense.

---
+++ MapView/src/GenConfig.h
@@ -0,0 +1,10 @@
+//Configuration parameters and constants
+//NOTE: Do not edit GenConfig.h directly; modify GenConfig.h.in instead and re-run cmake.
+#pragma once
+
+//Cmake-related constants
+#define MAPVIEW_VERSION_MAJOR ""
+#define MAPVIEW_VERSION_MINOR ""
+
+//Can add more cmake-imported flags here, as required.
+
