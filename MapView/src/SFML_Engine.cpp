18f94e17e49418543a11dc7434dd3ecb752b0fca
Seth N. Hetu <seth.hetu@gmail.com>
2011-12-31 14:03:44 +0800

    Sample SFML_Engine_Test to help us narrow down linker errors (and test the C++ app just in general). Linking fails.

--- MapView/src/SFML_Engine.cpp
+++ MapView/src/SFML_Engine.cpp
@@ -1,121 +1,15 @@
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-
-
-#include <list>
-#include <iostream>
-#include <sstream>
-#include <string>
-
-using namespace std;
-
-//Windows export stuff
-#ifdef __WIN32
-#define DLLEXPORT __declspec(dllexport)
-#else
-#define DLLEXPORT
-#endif
-
-
-//Prototype shared library functions
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-//Core functionality
-DLLEXPORT int init_sfml(int width, int height, int depth);
-DLLEXPORT int sfml_handle_events();
-DLLEXPORT void demo_display();
-DLLEXPORT void sfml_display();
-DLLEXPORT void close_sfml();
-
-//Post FX
-DLLEXPORT int can_use_postfx();
-DLLEXPORT sf::PostFX* new_postfx(const char* filename);
-DLLEXPORT void demo_set_default_postfx(sf::PostFX* item);
-DLLEXPORT void demo_update_postfx_color(sf::PostFX* item);
-
-//Demo
-DLLEXPORT sf::Shape* demo_init_poly();
-
-//Image
-DLLEXPORT sf::Image* new_image(const char* filename);
-DLLEXPORT int image_get_width(const sf::Image* item);
-DLLEXPORT int image_get_height(const sf::Image* item);
-
-//Sprite
-DLLEXPORT sf::Sprite* new_sprite();
-DLLEXPORT void sprite_set_image(sf::Sprite* item, const sf::Image* img);
-DLLEXPORT void sprite_set_position(sf::Sprite* item, int x, int y);
-DLLEXPORT void sprite_set_center(sf::Sprite* item, int x, int y);
-DLLEXPORT float sprite_get_rotation(const sf::Sprite* item);
-DLLEXPORT void sprite_set_rotation(sf::Sprite* item, float angle);
-DLLEXPORT sf::Color* sprite_get_color(const sf::Sprite* item);
-DLLEXPORT void sprite_set_color(sf::Sprite* item, sf::Color* color);
-
-//Polygon
-DLLEXPORT sf::Color* poly_get_point_color(const sf::Shape* item, int pointID);
-DLLEXPORT void poly_set_point_color(sf::Shape* item, int pointID, const sf::Color* color);
-DLLEXPORT void poly_set_scale_x(sf::Shape* item, float scale);
-DLLEXPORT void poly_set_scale_y(sf::Shape* item, float scale);
-DLLEXPORT void poly_set_pos_x(sf::Shape* item, int pos);
-DLLEXPORT void poly_set_pos_y(sf::Shape* item, int pos);
-
-//Color
-DLLEXPORT void color_set_red(sf::Color* item, int red);
-DLLEXPORT void color_set_green(sf::Color* item, int green);
-DLLEXPORT void color_set_blue(sf::Color* item, int blue);
-DLLEXPORT void color_set_alpha(sf::Color* item, int alpha);
-
-//Game functionality
-DLLEXPORT const sf::Input* game_get_input();
-DLLEXPORT int game_get_mouse_x();
-DLLEXPORT int game_get_mouse_y();
-DLLEXPORT float game_get_frame_time_s();
-DLLEXPORT void game_draw_item(sf::Drawable* item);
-
-//Cleanup
-DLLEXPORT void game_del_item(sf::Drawable* item);
-DLLEXPORT void game_del_color(sf::Color* color);
-DLLEXPORT void game_del_image(sf::Image* item);
-
-
-
-#ifdef __cplusplus
-}
-#endif
-
-
+#include "SFML_Engine.hpp"
 
 //+1 global variables!
 sf::RenderWindow myWindow;
 
+//We could even remove this too, but I don't see the need right now.
+sf::String fps;
 
+//This should stay in the engine.
+RollingAverage framerate(1000);
+sf::Clock frClock;
 
-class RollingAverage {
-public:
-	RollingAverage(unsigned int sampleSize) : sampleSize(sampleSize), runningTotal(0) {}
-
-	void addSample(float val) {
-		runningTotal += val;
-
-		sample.push_back(val);
-		if (sample.size()>sampleSize) {
-			runningTotal -= sample.front();
-			sample.pop_front();
-		}
-	}
-
-	float getAverage() {
-		return runningTotal/sample.size();
-	}
-
-private:
-	std::list<float> sample;
-	unsigned int sampleSize;
-	float runningTotal;
-};
 
 
 
@@ -137,15 +31,6 @@ bool handleEvent(const sf::Event& event)
 
 	return false;
 }
-
-
-//We could even remove this too, but I don't see the need right now.
-sf::String fps;
-
-//This should stay in the engine.
-RollingAverage framerate(1000);
-sf::Clock frClock;
-
 
 
 int init_sfml(int width, int height, int depth)
@@ -444,15 +329,3 @@ void main_loop_hack()
 
 	return;
 }
-
-
-//#ifndef IS_BUILDING_LIBRARY
-//
-//int main(int argc, char** argv)
-//{
-//	main_loop_hack();
-//	return 0;
-//}
-//
-//#endif //IS_BUILDING_LIBRARY
-
---
+++ MapView/src/SFML_Engine_Test.cpp
@@ -0,0 +1,14 @@
+//Simple file to help test our engine.
+// Exists mainly to check if linking succeeds or fails.
+#include "SFML_Engine.hpp"
+
+
+int main(int argc, char** argv)
+{
+	//TODO: Better usage example!
+	init_sfml(100, 100, 32);
+
+	return 0;
+}
+
+
